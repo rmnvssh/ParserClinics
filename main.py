82bdb694a82737db341cfe10458aff4a0d4545f0
alromanova <aaromanova_12@edu.hse.ru>
2022-06-07 00:48:18 +0300

    add csvfile

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as BS
+import random_info
+import pandas as pd
 
 page = 1
+id = 0
 filteredClinics = []
 
 while True:
@@ -14,26 +17,33 @@ while True:
 
     if len(allClinics):
         for clinic in allClinics:
+            id += 1
             isName = clinic.find('div', class_='b-card__name').\
                 find('a', class_='b-card__name-link b-link ui-text ui-text_h5 ui-text_color_primary-blue')
             name = next(isName.stripped_strings)
             address = clinic.find('div', class_='b-card__address-container').find('div', class_='b-card__address').text
             isPhone = clinic.find('div', class_='d-flex').find('div', class_='b-card__lpu-phone-container')
             if (isPhone is not None):
-                phone = isPhone.find('div', class_='b-card__lpu-phone').find('span', class_='b-card__lpu-phone-num').text
+                phone = isPhone.find('div', class_='b-card__lpu-phone').\
+                    find('span', class_='b-card__lpu-phone-num').text
             else:
-                phone = str("-")
+                phone = random_info.number()
 
-            link = clinic.find('div', class_='b-card__name').find('a', class_='b-card__name-link b-link ui-text ui-text_h5 ui-text_color_primary-blue').get('href')
+            mail = random_info.email_clinic(8)
+            passClinic = random_info.generate_password()
+
+            link = clinic.find('div', class_='b-card__name').\
+                find('a', class_='b-card__name-link b-link ui-text ui-text_h5 ui-text_color_primary-blue').get('href')
             clinicUrl = "https://prodoctorov.ru" + str(link)
             clinicR = requests.get(clinicUrl)
             title = BS(clinicR.text, "html.parser")
-            infoClinic = title.find('div', class_='b-clinic-profile__content b-text-unit b-text-unit_color_dark').get_text('\n', strip=False)
-            filteredClinics.append('Название клиники: ' + name + "\n" 'Адрес: ' + address + "\n" + 'Телефон: ' + phone + "\n" + infoClinic + "\n\n")
+            infoClinic = title.find('div', class_='b-clinic-profile__content b-text-unit b-text-unit_color_dark').\
+                get_text(' ', strip=False)
+            filteredClinics.append([str(id), name, infoClinic, address, mail, phone, str(passClinic)])
 
         page += 1
     else:
         break
 
-for data in filteredClinics:
-    print(data)
+hospitals = pd.DataFrame(filteredClinics, columns=['id[pk]', 'Название клиники', 'Специализация', 'Адрес', 'Email', 'Телефон', 'Пароль'])
+hospitals.to_csv('hospitals.csv', index=False, sep=';')
